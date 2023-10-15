ef9df1e35f0879ad337aba1b19381f0c886fbf7f
kalost07 <k.sav4ev@gmail.com>
2023-10-15 14:04:44 +0300

    FPS implemented

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,27 @@
 
 World world;
 
+static Uint32 next_time;
+static const int TICK_INTERVAL = 1000 / FPS;
+
+Uint32 time_left()
+{
+	Uint32 now;
+
+	now = SDL_GetTicks();
+	if (next_time <= now) return 0;
+	else return next_time - now;
+}
+
 int main(int* argc, char* argv[])
 {
 	world.init();
+	next_time = SDL_GetTicks() + TICK_INTERVAL;
 	while (world.isRunning())
 	{
 		world.run();
+		SDL_Delay(time_left());
+		next_time+=TICK_INTERVAL;
 	}
 
 	return 0;
